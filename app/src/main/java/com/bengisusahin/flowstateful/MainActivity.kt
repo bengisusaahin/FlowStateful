5736807e3c9cc746f3645d09afc577da49c8894a
Bengisu Şahin <74653216+bengisusaahin@users.noreply.github.com>
2024-07-25 15:09:00 +0300

    feat: viewmodel

--- app/src/main/java/com/bengisusahin/flowstateful/MainActivity.kt
+++ app/src/main/java/com/bengisusahin/flowstateful/MainActivity.kt
@@ -5,14 +5,12 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
@@ -31,7 +29,10 @@ class MainActivity : ComponentActivity() {
                     val navController = rememberNavController()
                     NavHost(navController = navController, startDestination = "firsScreen" ){
                         composable("firsScreen"){
-                            FirstScreen(time = 10, onButtonClicked = {
+                            val viewModel = viewModel<FirstScreenViewModel>()
+                            val time by viewModel.counter.collectAsState()
+
+                            FirstScreen(time = time, onButtonClicked = {
                                 navController.navigate("secondScreen")
                             })
                         }
